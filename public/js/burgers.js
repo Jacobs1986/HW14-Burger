525f9d1f5dff416c19ec106744ccd238880cbec5
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-15 14:29:18 -0600

    Create burgers.js
    Began the code to post the new burger to the database.

---
+++ public/js/burgers.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    // create a new burger
+    $(".create-form").on("submit", (event) => {
+        event.preventDefault();
+        console.log("Submit button clicked")
+    })
+
+})
