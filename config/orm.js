a626cc60731ae45846b230a9837564eaf2b50dce
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-15 13:21:31 -0600

    Update orm.js
    Wrote in the code to connect with the database.

---
+++ config/orm.js
@@ -0,0 +1,17 @@
+// Import MySQL connection
+const connection = require("../config/connection.js");
+
+// Object for all our SQL 
+let orm = {
+    all: function(tableInput, cb) {
+        let queryString = "SELECT * FROM ??;";
+        connection.query(queryString, [tableInput], function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+}
+
+module.exports = orm;
