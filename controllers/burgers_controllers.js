397435fc9c5b691bf0e990523595592b0c1c5a96
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-15 13:36:22 -0600

    Updated burgers_controller and server
    Put the code in each to link them together

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+// import the model
+let burger = require("../models/burger.js");
+
+// display all the burgers from the database
+router.get("/", function(request, result) {
+    burger.all(function(data) {
+        let hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        result.render("index", hbsObject);
+    })
+})
