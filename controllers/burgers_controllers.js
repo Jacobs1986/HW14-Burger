b47adb9a5ad216a26e27306d119fcda8a7bb7611
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-16 16:51:52 -0600

    Update burgers_controllers.js
    I put into it devoured=1 because that is what we need to switch too

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -6,8 +6,8 @@ const router = express.Router();
 let burger = require("../models/burger.js");
 
 // display all the burgers from the database
-router.get("/", function(request, result) {
-    burger.all(function(data) {
+router.get("/", function (request, result) {
+    burger.all(function (data) {
         let hbsObject = {
             burger: data
         };
@@ -16,20 +16,27 @@ router.get("/", function(request, result) {
     })
 })
 // add a new burger
-router.post("/api/burgers", function(request, response) {
+router.post("/api/burgers", function (request, response) {
     console.log(request.body);
-    burger.create(["burger_name"], [request.body.name], function(result) {
+    burger.create(["burger_name"], [request.body.name], function (result) {
         response.json({ id: result.insertId });
     })
 })
 
 // update eat state of burger
-router.put("/api/burgers/:id", function(request, response) {
+router.put("/api/burgers/:id", function (request, response) {
     let condition = `id = ${request.params.id}`;
     console.log("condition", condition);
-    burger.update({
-        devoured: request.body.devoured
-    })
+    burger.update(
+        "devoured=1",
+        condition,
+        function (result) {
+            if (result.changedRows === 0) {
+                return response.status(404).end();
+            }
+            response.status(200).end();
+        }
+    )
 })
 
 module.exports = router;
